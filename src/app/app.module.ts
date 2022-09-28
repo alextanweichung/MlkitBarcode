fe3b3e5964ad8dfc9ee828b5fea7a6dbc72245fe
KH <kh@idcp.my>
2022-09-28 16:08:59 +0800

    Initial Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgChartsModule } from 'ng2-charts';
+import { Storage  } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot({ mode: 'ios' }), 
+    ReactiveFormsModule, 
+    AppRoutingModule, 
+    NgChartsModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Storage],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
