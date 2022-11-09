f3fb5792350afd13c109ea0c2d359916985ab844
aychia <aychia@idcp.my>
2022-11-09 12:32:07 +0800

    calendar input with time

--- src/app/shared/pages/calendar-input/calendar-input.page.ts
+++ src/app/shared/pages/calendar-input/calendar-input.page.ts
@@ -11,18 +11,27 @@ export class CalendarInputPage implements OnInit {
   @Input() title: string = 'Date';
   @Input() defaultDate: Date = new Date();
   @Input() labelPosition = 'stacked';
+  @Input() presentation = 'date';
   @Output() onDateSelected: EventEmitter<Date> = new EventEmitter();
 
   date_active: boolean = false;
   date_display: string;
+  date_value_for_ion_calendar: Date;
   date_value: Date;
 
   constructor() { }
 
   ngOnInit() {
     if (!this.date_value) {
-      this.date_value = this.defaultDate;
-      this.date_display = format(parseISO(this.date_value.toISOString()), 'MMM d, yyyy');
+      if (this.presentation === 'date-time') {
+        this.date_value = new Date(this.defaultDate.getFullYear(), this.defaultDate.getMonth(), this.defaultDate.getDate(), this.defaultDate.getHours(), this.defaultDate.getMinutes(), 0);
+        this.date_value_for_ion_calendar = new Date(this.defaultDate.getFullYear(), this.defaultDate.getMonth(), this.defaultDate.getDate(), this.defaultDate.getHours(), this.defaultDate.getMinutes(), 0);
+      } else {
+        this.date_value = new Date(this.defaultDate.getFullYear(), this.defaultDate.getMonth(), this.defaultDate.getDate(), 0, 0, 0);
+        this.date_value_for_ion_calendar = new Date(this.defaultDate.getFullYear(), this.defaultDate.getMonth(), this.defaultDate.getDate(), 0, 0, 0);
+      }
+      this.date_value_for_ion_calendar.setMinutes(this.date_value_for_ion_calendar.getMinutes() - this.date_value_for_ion_calendar.getTimezoneOffset());
+      this.date_display = this.presentation === 'date-time' ? format(this.date_value, 'dd/MM/yyyy hh:mm aa') : format(this.date_value, 'dd/MM/yyyy');
       this.onDateSelected.emit(this.date_value);
     }
   }
@@ -36,16 +45,32 @@ export class CalendarInputPage implements OnInit {
   // On date from select
   onDateSelect(event: any) {
     let date = new Date(event.detail.value);
-    this.date_value = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0));
-    this.date_display = format(parseISO(event.detail.value), 'MMM d, yyyy');
-    this.date_active = false;
+    this.date_active = false
+    if (this.presentation === 'date-time') {
+      this.date_value = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), 0);
+      this.date_value_for_ion_calendar = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), 0);
+    } else {
+      this.date_value = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
+      this.date_value_for_ion_calendar = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
+    }
+    this.date_value_for_ion_calendar.setMinutes(this.date_value_for_ion_calendar.getMinutes() - this.date_value_for_ion_calendar.getTimezoneOffset());
+    this.date_display = this.presentation === 'date-time' ? format(this.date_value, 'dd/MM/yyyy hh:mm aa') : format(this.date_value, 'dd/MM/yyyy');
+    console.log("🚀 ~ file: calendar-input.page.ts ~ line 59 ~ CalendarInputPage ~ onDateSelect ~ this.date_value", this.date_value)
     this.onDateSelected.emit(this.date_value);
   }
 
   resetControl() {
     this.date_active = false;
-    this.date_value = this.defaultDate;
-    this.date_display = format(parseISO(this.date_value.toISOString()), 'MMM d, yyyy');    
+    this.defaultDate = new Date();
+    if (this.presentation === 'date-time') {
+      this.date_value = new Date(this.defaultDate.getFullYear(), this.defaultDate.getMonth(), this.defaultDate.getDate(), this.defaultDate.getHours(), this.defaultDate.getMinutes(), 0);
+      this.date_value_for_ion_calendar = new Date(this.defaultDate.getFullYear(), this.defaultDate.getMonth(), this.defaultDate.getDate(), this.defaultDate.getHours(), this.defaultDate.getMinutes(), 0);
+    } else {
+      this.date_value = new Date(this.defaultDate.getFullYear(), this.defaultDate.getMonth(), this.defaultDate.getDate(), 0, 0, 0);
+      this.date_value_for_ion_calendar = new Date(this.defaultDate.getFullYear(), this.defaultDate.getMonth(), this.defaultDate.getDate(), 0, 0, 0);
+    }
+    this.date_value_for_ion_calendar.setMinutes(this.date_value_for_ion_calendar.getMinutes() - this.date_value_for_ion_calendar.getTimezoneOffset());
+    this.date_display = this.presentation === 'date-time' ? format(this.date_value, 'dd/MM/yyyy hh:mm aa') : format(this.date_value, 'dd/MM/yyyy');
   }
 
 }
