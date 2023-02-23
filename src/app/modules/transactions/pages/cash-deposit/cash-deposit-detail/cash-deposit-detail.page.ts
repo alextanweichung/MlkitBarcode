c58a379e92fe825e7286b5b429c04c95845134dc
aychia <aychia@idcp.my>
2023-02-23 14:27:48 +0800

    show bigger attchament

--- src/app/modules/transactions/pages/cash-deposit/cash-deposit-detail/cash-deposit-detail.page.ts
+++ src/app/modules/transactions/pages/cash-deposit/cash-deposit-detail/cash-deposit-detail.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ActivatedRoute, NavigationExtras } from '@angular/router';
 import { NavController, ModalController } from '@ionic/angular';
@@ -79,6 +79,74 @@ export class CashDepositDetailPage implements OnInit {
       }
     }
     this.navController.navigateForward('/transactions/cash-deposit/cash-deposit-edit', navigationExtras);
+  }
+
+  scale = 1;
+
+  zoomIn() {
+    this.scale += 0.1;
+    this.setTransform();
+  }
+
+  zoomOut() {
+    if (this.scale > 1) {
+      this.scale -= 0.1;
+      this.setTransform();
+    }
+  }
+
+  @ViewChild('imageEle', {static:false}) imageEle: ElementRef;
+  setTransform() {
+    // const image = document.querySelector('.image-container img');
+    // image.style.transform = `scale(${this.scale})`;
+    const image = this.imageEle.nativeElement;
+    image.style.transform = `scale(${this.scale})`;
+  }
+
+  onDoubleClick() {
+    const image = this.imageEle.nativeElement;
+    if (this.scale === 1) {
+      this.scale = 1.5;
+      image.style.transform = `scale(${this.scale})`;
+    } else {
+      this.scale = 1;
+      image.style.transform = `scale(${this.scale})`;
+    }
+  }
+
+  initialTouchDistance = null;
+  initialScale = 1;
+  currentScale = 1;
+  onTouchStart(event) {
+    if (event.touches.length > 1) {
+      const touch1 = event.touches[0];
+      const touch2 = event.touches[1];
+      this.initialTouchDistance = this.getDistance(touch1, touch2);
+    }
+  }
+
+  onTouchMove(event) {
+    if (event.touches.length > 1) {
+      const touch1 = event.touches[0];
+      const touch2 = event.touches[1];
+      const distance = this.getDistance(touch1, touch2);
+      if (this.initialTouchDistance) {
+        this.initialScale = this.currentScale;
+        const initialDistance = this.initialTouchDistance;
+        this.currentScale = this.initialScale * (distance / initialDistance);
+        this.imageEle.nativeElement.style.transform = `scale(${this.currentScale})`;
+      }
+    }
+  }
+
+  onTouchEnd(event) {
+    this.initialTouchDistance = null;
+  }
+
+  getDistance(point1, point2) {
+    const dx = point1.clientX - point2.clientX;
+    const dy = point1.clientY - point2.clientY;
+    return Math.sqrt(dx * dx + dy * dy);
   }
 
 }
