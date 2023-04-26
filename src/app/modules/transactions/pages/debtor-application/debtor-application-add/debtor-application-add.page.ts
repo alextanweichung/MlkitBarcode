c437d221f7adc3f6d308b4c9bd0ddcf6150ea7ef
aychia <aychia@idcp.my>
2023-04-26 17:28:50 +0800

    debtor application bugs

--- src/app/modules/transactions/pages/debtor-application/debtor-application-add/debtor-application-add.page.ts
+++ src/app/modules/transactions/pages/debtor-application/debtor-application-add/debtor-application-add.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DebtorApplicationService } from '../../../services/debtor-application.service';
 import { ActionSheetController, AlertController, NavController } from '@ionic/angular';
 import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -31,19 +31,18 @@ export class DebtorApplicationAddPage implements OnInit {
 
   newObjectForm() {
     let loginUser = JSON.parse(localStorage.getItem('loginUser'));
-    console.log("🚀 ~ file: debtor-application-add.page.ts:34 ~ DebtorApplicationAddPage ~ newObjectForm ~ loginUser:", loginUser)
     this.objectForm = this.formBuilder.group({
       customerPreId: [0],
-      customerPreCode: [null, [Validators.required, Validators.maxLength(100)]],
+      customerPreCode: [null, [Validators.maxLength(100)]],
       businessModelType: [null],
       name: [null, [Validators.required, Validators.maxLength(100)]],
-      companyRegNum: [null, [Validators.required, Validators.maxLength(20)]],
+      companyRegNum: [null, [Validators.maxLength(20)]],
       oldCustomerCode: [null],
       supplyTaxId: [null],
       taxNum: [null, [Validators.maxLength(20)]],
       currencyId: [null],
-      stateId: [null, [Validators.required]],
-      countryId: [null, [Validators.required]],
+      stateId: [null],
+      countryId: [null],
       paymentMethodId: [null],
       bankAcct: [null, [Validators.maxLength(100)]],
       controlAccountId: [null, [Validators.maxLength(20)]],
@@ -64,15 +63,15 @@ export class DebtorApplicationAddPage implements OnInit {
       shipPhone: [null, [Validators.maxLength(100)]],
       shipFax: [null, [Validators.maxLength(100)]],
       billDescription: [null, [Validators.maxLength(100)]],
-      billAddress: [null, [Validators.required, Validators.maxLength(500)]],
+      billAddress: [null, [Validators.maxLength(500)]],
       billPostCode: [null, [Validators.maxLength(10)]],
       billAreaId: [null, [Validators.maxLength(20)]],
-      billStateId: [null, [Validators.required]],
+      billStateId: [null],
       shipDescription: [null, [Validators.maxLength(100)]],
-      shipAddress: [null, [Validators.required, Validators.maxLength(500)]],
+      shipAddress: [null, [Validators.maxLength(500)]],
       shipPostCode: [null, [Validators.maxLength(10)]],
       shipAreaId: [null, [Validators.maxLength(20)]],
-      shipStateId: [null, [Validators.required]],
+      shipStateId: [null],
       masterUDGroup1: [null],
       customerUDField1: [null, [Validators.maxLength(100)]],
       customerUDField2: [null, [Validators.maxLength(100)]],
@@ -162,9 +161,9 @@ export class DebtorApplicationAddPage implements OnInit {
   }
 
   async nextStep() {
-    try {
-      console.log("🚀 ~ file: debtor-application-add.page.ts:186 ~ DebtorApplicationAddPage ~ nextStep ~ this.objectForm:", this.objectForm)      
-      if (this.objectForm.valid) {
+    console.log("🚀 ~ file: debtor-application-add.page.ts:186 ~ DebtorApplicationAddPage ~ nextStep ~ this.objectForm:", this.objectForm)
+    if (this.objectForm.valid) {
+      try {
         const alert = await this.alertController.create({
           header: 'Are you sure to proceed?',
           buttons: [
@@ -184,11 +183,9 @@ export class DebtorApplicationAddPage implements OnInit {
           ],
         });
         await alert.present();
-      } else {
-        this.toastService.presentToast('Error', 'Please fill required fields & attach 1 file to continue', 'top', 'danger', 2000);
+      } catch (e) {
+        console.error(e);
       }
-    } catch (e) {
-      console.error(e);
     }
   }
 
