52e8e1f271564a3381a13231d0fbbefc5fd0164f
aychia <aychia@idcp.my>
2023-05-03 16:52:01 +0800

    new loading logic qty in cart UI changes

--- src/app/services/loading/loading.service.ts
+++ src/app/services/loading/loading.service.ts
@@ -14,21 +14,25 @@ export class LoadingService {
   ) { }
 
   async showLoading(message: string = 'Loading') {
-    this.loading = await this.loadingController.create({
-      cssClass: 'default-loading',
-      message: `<p>${message}...</p><span>Please be patient.</span>`,
-      spinner: 'crescent',
-      backdropDismiss: true
-    });
     if (!this.isShowing) {
       this.isShowing = true;
+      this.loading = await this.loadingController.create({
+        cssClass: 'default-loading',
+        message: `<p>${message}...</p><span>Please be patient.</span>`,
+        spinner: 'crescent',
+        backdropDismiss: true 
+      });
       await this.loading.present();
-    }
+    } else {
+      // If loader is showing, only change text, won't create a new loader.
+      this.isShowing = true;
+      this.loading.message = message;
+  }
   }
 
-  async dismissLoading() {
-    this.loading?.dismiss();
+  async dismissLoading() {    
     this.isShowing = false;
+    this.loading?.dismiss();
   }
 
 }
