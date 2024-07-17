e3f989880f1cce52ed607e0b35088dddff10b6c3
aychia <aychia@idcp.my>
2024-07-17 17:27:12 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,20 +77,21 @@ export class AppComponent {
 
 function OneSignalInit() {
    // Uncomment to set OneSignal device logging to VERBOSE  
-   OneSignal.setLogLevel(6, 0);
+   OneSignal.Debug.setLogLevel(6);
    
    // NOTE: Update the setAppId value below with your OneSignal AppId.
-   OneSignal.setAppId("7f373743-31c5-49d7-b2ba-bffb45ab191e");
+   OneSignal.initialize("7f373743-31c5-49d7-b2ba-bffb45ab191e");
 
-   OneSignal.setNotificationOpenedHandler(function (jsonData) {
-      console.log("notificationOpenedCallback: " + JSON.stringify(jsonData));
-   });
+   OneSignal.Notifications.addEventListener("click", async (e) => {
+      let clickData = await e.notification;
+      console.log("Notification Clicked : " + clickData);
+   })
 
    // Prompts the user for notification permissions.
    //    * Since this shows a generic native prompt, we recommend instead using an In-App Message to prompt for notification permission (See step 7) to better communicate to your users what notifications they will get.
    
-   OneSignal.promptForPushNotificationsWithUserResponse(function (accepted) {
-      console.log("User accepted notifications: " + accepted);
-   });
+   OneSignal.Notifications.requestPermission(true).then((success: Boolean) => {
+      console.log("Notification permission granted " + success);
+    })
 }
 
