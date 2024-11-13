9701c8d5d880a37b12e627cc706c8b33cd2a66a3
Alex <alextanweichung@gmail.com>
2024-11-13 11:49:05 +0800

    test7

--- src/app/shared/pages/barcode-scan-input/barcode-scan-input.module.ts
+++ src/app/shared/pages/barcode-scan-input/barcode-scan-input.module.ts
@@ -8,7 +8,7 @@ import { SharedTestingModule } from '../../../../tests/modules/shared-testing.mo
 import { BarcodeScanInputPageRoutingModule } from './barcode-scan-input-routing.module';
 import { BarcodeScanningModalComponent } from './barcode-scan-input.component';
 
-import { BarcodeScanInputPage } from "./barcode-scan-input.page";
+import { BarcodeScanInputPage } from './barcode-scan-input.page';
 import { IdMappingModule } from "../../pipes/id-mapping/id-mapping.module";
 import HideKeyboardModule from '../../utilities/hide-keyboard.module';
 import { IdToCodeMappingModule } from "../../pipes/id-to-code-mapping/id-to-code-mapping.module";
@@ -25,6 +25,7 @@ import { IdToCodeMappingModule } from "../../pipes/id-to-code-mapping/id-to-code
         SharedTestingModule
     ],
     declarations:[BarcodeScanInputPage, BarcodeScanningModalComponent],
+    exports: [BarcodeScanInputPage]
 })
 
 export class BarcodeScanInputPageModule{
